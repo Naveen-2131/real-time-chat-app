f0a38e753fc648e2ca0cd24f8b689379f471dbdf
Naveen-2131 <naveen8870002032@gmail.com>
2025-12-04 22:25:39 +0530

    Update messageRoutes.js

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -9,6 +9,11 @@ const { protect } = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 
 
+console.log('--- DEBUG MESSAGE ROUTES ---');
+console.log('protect type:', typeof protect);
+console.log('upload type:', typeof upload);
+console.log('sendMessage type:', typeof sendMessage);
+
 router.route('/').post(protect, (req, res, next) => {
     console.log('🔥🔥🔥 MESSAGE ROUTE HIT 🔥🔥🔥');
     console.log('Headers:', req.headers);
