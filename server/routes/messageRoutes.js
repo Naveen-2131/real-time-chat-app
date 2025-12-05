32998019aa98b8e7a8cd8d95e907113cc3f5fcc3
Naveen-2131 <naveen8870002032@gmail.com>
2025-12-05 10:47:23 +0530

    Update messageRoutes.js

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -4,23 +4,16 @@ const {
     sendMessage,
     allMessages,
     allGroupMessages,
+    markAsRead,
 } = require('../controllers/messageController');
 const { protect } = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 
 
-console.log('--- DEBUG MESSAGE ROUTES ---');
-console.log('protect type:', typeof protect);
-console.log('upload type:', typeof upload);
-console.log('sendMessage type:', typeof sendMessage);
-
-router.route('/').post(protect, (req, res, next) => {
-    console.log('🔥🔥🔥 MESSAGE ROUTE HIT 🔥🔥🔥');
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
-    next();
-}, upload, sendMessage);
+router.route('/').post(protect, upload, sendMessage);
 router.route('/conversation/:conversationId').get(protect, allMessages);
 router.route('/group/:groupId').get(protect, allGroupMessages);
+router.route('/mark-read/conversation/:conversationId').put(protect, markAsRead);
+router.route('/mark-read/group/:groupId').put(protect, markAsRead);
 
 module.exports = router;
